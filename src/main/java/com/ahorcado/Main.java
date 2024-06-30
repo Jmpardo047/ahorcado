48a9839a13456cd78604be70a25eb2e96bb66597
Jmpardo047 <140624378+Jmpardo047@users.noreply.github.com>
2024-06-30 17:45:57 -0500

    updates

--- src/main/java/com/ahorcado/Main.java
+++ src/main/java/com/ahorcado/Main.java
@@ -20,47 +20,50 @@ public class Main {
         boolean found = false;
         int tries = 3;
         try (Scanner sc = new Scanner(System.in)){
-            clearConsole();
             for(int i=0; i<choose.length();i++){
                 hang.add("_ ");
             }
-            System.out.println(hang);
             while (isActive) {
+                found = false;
+                for (String i : hang) {
+                    System.out.print(i);
+                }
+                System.out.println("");
                 letter = sc.nextLine();
-                for (int i = 0; i<choose.length();i++){
-                    if (letter.charAt(0) == choose.toLowerCase().charAt(i)) {
-                        hang.set(i,letter);
-                        found = true;
-                        System.out.println(hang);
-                        clearConsole();
+                if (letter.length() == 1) {
+                    for (int i = 0; i<choose.length();i++){
+                        if (letter.charAt(0) == choose.toLowerCase().charAt(i)) {
+                            hang.set(i,letter);
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Letra incorrecta");
+                        tries --;
+                    }
+                    if (tries <= 0) {
+                        System.out.println("Perdiste");
+                        isActive = !isActive;
+                    }
+                    if(!hang.contains("_ ")){
+                        System.out.println("ganaste");
+                        isActive = !isActive;
                     }
                 }
-                if (!found) {
-                    System.out.println("Letra incorrecta");
-                    tries --;
-                }
-                if (tries <= 0) {
-                    System.out.println("Perdiste");
-                    isActive = !isActive;
-                }
-                if(!hang.contains("_ ")){
-                    System.out.println("ganaste");
-                    isActive = !isActive;
-                }
-                clearConsole();
+
             }
-            // Object[] hangArray = hang.toArray();
-            
         }
     }
 
     public static void clearConsole() {
         try {
-            if (System.getProperty("os.name").contains("windows")) {
-                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            if(System.getProperty("os.name").contains("Window")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
             }
-        } catch (IOException | InterruptedException ex) {
-            ex.printStackTrace();
+        }catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
         }
     }
 }
